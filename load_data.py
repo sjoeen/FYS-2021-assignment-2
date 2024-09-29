08be70225f1b381229e1bc553c8036afa6df676f
sjoeen <149700123+sjoeen@users.noreply.github.com>
2024-09-29 19:08:17 +0200

    finished assignment

--- load_data.py
+++ load_data.py
@@ -4,52 +4,87 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from prediction_model import Gaussian
 from prediction_model import Gamma
+from prediction_model import BayesClassifier
 
 
 
 
-def load_data(file_name):
+def _load_data(file_name):
+    """
+    A helping function that opens a file
+    """
     data = pd.read_csv(file_name, sep=',',header=None)
 
     return data.to_numpy()
 
 
+
+
+def classify(file_name):
+    """
+    This function splits the classes using their labels.
+    """
+
+    dataset = _load_data(file_name) 
+ 
+    labels = dataset[1, :] 
+    values = dataset[0, :]  
+    class_0 = values[labels == 0]  
+    class_1 = values[labels == 1]
+        #Defining the classes
+
+    return class_0,class_1
+
+
+
+
 if __name__ == "__main__":
 
-    dataset = load_data("data_problem2.csv")
-    #print(dataset)
 
-    class_0 = dataset[0, :]  
-    class_1 = dataset[1,:]
+    class_gamma,class_gaussian = classify("data_problem2.csv")
+
+
+
+    """
+    Cleaning up,splitting and giving labels to the classes in the dataset.
+    """
+
+    gamma_train,gamma_test = train_test_split(class_gamma,test_size=0.2)
+    gaussian_train,gaussian_test = train_test_split(class_gaussian,test_size=0.2)
+    gamma_test = np.column_stack((gamma_test, np.zeros(gamma_test.shape[0])))
+        #giving labels back
+    gaussian_test = np.column_stack((gaussian_test, np.ones(gaussian_test.shape[0])))
+        #giving labels back
+    test_set = np.concatenate((gamma_test,gaussian_test))
+        #adding the distrubutions into one test set
+    test_data = test_set[:, 0]  
+    test_labels = test_set[:, 1]  
+        #splitting labels and datapoint (X and Y)
+
+    """
+    Defining the classes:
+    """
+
+    gassuian_prior = 2000/3600
+    gamma_prior = 1600/3600
+
+    gaussian = Gaussian(gaussian_train,gassuian_prior)
+    gamma = Gamma(gamma_train,gamma_prior)
+    class_list = [gamma,gaussian]
+    bayes = BayesClassifier(test_data,class_list,test_labels)
+    print(bayes.compare())
+    missclassified = (bayes.miscalulation())
+
+    """
+    plot data and missclassified samples.
+    """
 
     plt.figure(figsize=(10,6))
-
-    # Plot class 0
-    plt.hist(class_0, bins=10, alpha=0.5, label="Class 0")
-
-    # Plot class 1
-    plt.hist(class_1, bins=10, alpha=0.5, label="Class 1")
-
-    # Add labels and title
+    plt.hist(class_gamma, bins=10, alpha=0.5, label="Class 0")
+    plt.hist(class_gaussian, bins=10, alpha=0.5, label="Class 1")
+    plt.hist(missclassified, bins=10, alpha=1, label="miss-classification")
     plt.xlabel("Class value")
     plt.ylabel("frequency")
     plt.title('Histogram of class 0 and class 1')
     plt.legend()
-
-    # Show the plot
     plt.show()
-    
-    class_0_train,class_0_test = train_test_split(class_0,test_size=0.2,random_state=21)
-    class_1_train,class_1_test = train_test_split(class_1,test_size=0.2,random_state=21)
-
-    test_set = np.concatenate((class_0_test,class_1_test))
-    
-    a = Gaussian(class_0_train)
-    #print(a.var)
-    print(a.log_likelihood(test_set))
-    b = Gamma(class_1_train)
-    print(b.beta)
-    print(b.log_likelihood(test_set))
-
-
-
--- prediction_model.py
+++ prediction_model.py
@@ -3,12 +3,13 @@ import numpy as np
 
 class Gaussian:
 
-    def __init__(self,x):
+    def __init__(self,x,prior):
 
         self.x = x
         self.N = len(x)
         self.mean = x.mean()
         self.var = 0
+        self.prior = prior
 
 
         for i in range(self.N):
@@ -24,40 +25,110 @@ class Gaussian:
         set of values is under our gaussian distribution. Proof of this formula is under
         point 2.2.
         """
-
-
         predictions = np.array(predictions,dtype=np.float64)
         
-        prefactors = -np.log(self.std * (np.sqrt  (2*np.pi) ) )
-        exponent = (-1/2) * ((predictions-self.mean)/self.std)**2
-        return prefactors + exponent
+        term1 = -np.log(self.std * (np.sqrt  (2*np.pi) ) )
+        term2 = (-1/2) * ((predictions-self.mean)/self.std)**2
+        log_likelihoods = term1 + term2 + np.log(self.prior)
+
+        return log_likelihoods
 
 class Gamma:
 
-    def __init__(self,x,gamma=2):
+    def __init__(self,x,prior,gamma=2):
 
         self.x = x
         self.N = len(x)
         self.gamma = gamma
         self.beta = 0
+        self.prior = prior
 
         for i in range(self.N):
             self.beta += x[i]
         self.beta /= ( self.N * self.gamma )
 
     def log_likelihood(self,predictions):
-
+        """
+        This function takes the log likelihood in order to check how likely it is that a 
+        set of values is under our gamma distribution. Proof of this formula is under
+        point 2.2.
+        """
         predictions = np.array(predictions,dtype=np.float64)
         predictions = np.clip(predictions, 1e-8, None)
+            #gamma distrubution can't handle negative values
+            #chatgpt helped with this cmd when problem shooting. 
 
-        prefactors = -2*self.N *np.log(self.beta) + np.log(predictions)
-        factor = (-1/self.beta) * predictions
+        term1 = self.gamma * np.log(self.beta)
+        term2 = np.log(self.gamma)
+        term3 = np.log(predictions)
+        term4 = - predictions  / self.beta
 
-        return prefactors + factor
+        log_likelihoods = self.N * (term1 + term2 + term3 + term4 + np.log(self.prior))
+
+        return log_likelihoods
 
 
 class BayesClassifier:
+    """
+    Bayes classifier class compares the log likelihood of each class
+    in the classes list and return the accuracy.
+    """
 
-    def __init__(self,x,y,classes):
+    def __init__(self,x,classes,y):
 
         self.x = x
+        self.num_samples = len(x)
+        self.classes = classes
+        self.y = y
+
+    def _compare(self):
+        """
+        A help function for the compare function.
+        This function returns a  numpy list of the most likely
+        class for each sample.
+        """
+
+        log_likelihoods = []
+
+        for model in self.classes:
+            predictions = model.log_likelihood(self.x)
+            log_likelihoods.append(predictions)
+
+        log_likelihoods = (np.column_stack(log_likelihoods))
+                #got help from chatgpt with this cmd
+
+        return np.argmax(log_likelihoods,axis=1)
+    
+    def compare(self):
+            """
+            A compare function that returns the accuracy by comparing
+            the samples to the labels.
+            """
+
+            predictions = self._compare()
+            correct_counter = 0
+            correct_list = (predictions == self.y) 
+
+            for pred in correct_list:
+
+                if pred:
+                    correct_counter+=1
+
+            accuracy = round(correct_counter / self.num_samples,4) 
+
+            return accuracy * 100
+    
+    def miscalulation(self):
+        """
+        A function that returns a list of misclassifications.
+        """
+        predictions = self._compare()
+        incorrect_list = (predictions != self.y)
+
+        data_points = []
+
+        for index,pred in enumerate(incorrect_list):
+            if pred:
+                data_points.append(self.x[index])
+
+        return data_points
